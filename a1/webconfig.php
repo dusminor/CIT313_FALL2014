c33a132f10aa5a09170f3e65cd368b9611a21557
dusminor <dusminor@iupui.edu>
2014-09-17 19:59:11 -0400

    Moved 'a1' folder

---
+++ a1/webconfig.php
@@ -0,0 +1,4 @@
+<?php
+//inserted absolute path and url root variable definitions
+define('ABSOLUTE_PATH', '/home/dusminor/CIT313/FA2014/a1');
+define('URL_ROOT', 'http://tomcat.cit.iupui.edu/dusminor/CIT313/FA2014/a1');
